8feb02c61e0bb5dc64d281922408ebb5c52a53a9
bylev <michelle.camara@tecdesoftware.edu.mx>
2026-06-03 07:24:34 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/PacienteController.cs
@@ -0,0 +1,6 @@
+﻿namespace CitasApp.Controllers
+{
+    public class PacienteController
+    {
+    }
+}
---
+++ Models/Paciente.cs
@@ -0,0 +1,12 @@
+﻿namespace CitasApp.Models
+{
+    public class Paciente
+    {
+        public int Id { get; set; } 
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string Email { get; set; }  = string.Empty;
+        public string Telegono { get; set; } = string.Empty;
+
+    }
+}
